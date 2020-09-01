0f4f18fe89f02356d3d6683d368d80e0cb32331c
GALLOWAY <bgallow@8c85904cf5d0.ant.amazon.com>
2020-09-01 10:35:03 -0400

    Created new ruby file.

---
+++ reg_expressions.rb
@@ -0,0 +1,31 @@
+# Using re's to break up a line.
+print "Please enter a line: "
+line = gets.chomp
+
+ssn = 111-11-111
+ccn = 5180-3807-3679-8221
+
+res = [ ]
+while res != ''
+    # String leading blanks.
+    line.sub!(/^\s*/, '')
+
+    break if line == ''
+
+    # See what the leading is for next action.
+    if line[0].chr == '"' then
+        # Quoted.
+        line.sub!(/^"([^"]*)"/, '')
+        res.push($1)
+    elsif line.sub!(/^(\d+):(\S+)/, '')
+        # Repeated with n:
+        $1.to_i.times { res.push($2) }
+    else
+        # Just a word.
+        line.sub!(/^(\S+)/, '')
+        res.push($1)
+    end
+
+end
+
+res.each { |x| print "  [", x, "]\n" }
